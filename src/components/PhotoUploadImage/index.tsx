ebcf773f86343712e31b664576fce75aaecf4e00
Arthur Diniz da Luz <arthurdiluz@outlook.com>
2023-11-02 09:10:32 -0300

    feat: add PhotoUploadImage component

---
+++ src/components/PhotoUploadImage/index.tsx
@@ -0,0 +1,28 @@
+import { Image, ImageSourcePropType, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import PhotoUploadIcon from "../Icons/PhotoUpload";
+import styles from "./styles";
+
+type Props = {
+  source?: ImageSourcePropType | null;
+  onPress?: () => void;
+};
+
+const PhotoUploadImage = ({ source, onPress }: Props) => {
+  return (
+    <TouchableOpacity style={styles.touchable} onPress={onPress}>
+      {source ? (
+        <Image source={source} style={styles.image} resizeMode="cover" />
+      ) : (
+        <PhotoUploadIcon
+          style={styles.image}
+          height={"90%"}
+          width={"90%"}
+          fill={"#FFF"}
+        />
+      )}
+    </TouchableOpacity>
+  );
+};
+
+export default PhotoUploadImage;
